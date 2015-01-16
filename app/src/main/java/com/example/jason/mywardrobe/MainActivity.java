f58f72446dcf826bf1b53e5a44b284aed043de44
Jason <jason5540105@yahoo.com.tw>
2015-01-17 01:28:08 +0800

    nothing has changed

--- app/src/main/java/com/example/jason/mywardrobe/MainActivity.java
+++ app/src/main/java/com/example/jason/mywardrobe/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,6 +23,12 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
+            Button btn = (Button)findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
         }
     }
 
